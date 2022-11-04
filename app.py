871cd2b5998b2b4f366488794ccea5028ece2c6b
genlikan <54910000+genlikan@users.noreply.github.com>
2022-11-04 19:27:35 -0400

    updated POST functions

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from ClientWrapper import ClientWrapper
 from panopto_oauth2 import PanoptoOAuth2
 from datetime import datetime, timedelta
 
+import datetime
 import asyncio
 
 server = "mitsloan.hosted.panopto.com"
@@ -28,39 +29,7 @@ SessionManagement_client = auth.get_client('SessionManagement')
 
 app = Flask(__name__)
 
-# recordings = [
-#     {   
-#         "id": 123456, 
-#         "Recording Information": {
-#             "Title": "Room E52-164 Classroom Recording",
-#             "Start Time": "11 1 2022 6 27",
-#             "Duration": "5400"
-#         }
-#     },
-#         {   
-#         "id": 654321, 
-#         "Recording Information": {
-#             "Title": "Room E52-221 Classroom Recording",
-#             "Start Time": "11 2 2022 6 8",
-#             "Duration": "5400"
-#         }
-#     }
-# ]
-
-# rooms = [
-#     {
-#         "name": "E52-164",
-#         "state": "Recording",
-#         "recording_id": 123456
-#     },
-#         {
-#         "name": "E52-221",
-#         "state": "Recording",
-#         "recording_id": 654321
-#     }
-# ]
-
-def get_recorder_dict():        # populate dict
+def get_recorder_dict():        # populate recorder:recorder_id dict
     Grand_Dict_ListofRecorders = RemoteRecorderManagement_client.call_service("ListRecorders",pagination=50)
     ListofRecorders = Grand_Dict_ListofRecorders["PagedResults"]["RemoteRecorder"]
     DOR = {}
@@ -72,13 +41,13 @@ def get_recorder_dict():        # populate dict
 rooms = get_recorder_dict()
 # print(rooms)
 
-def get_room_name(Id):          # s
+def get_room_name(Id):          # swap from room_name to room_id
     keys=[k for k, v in rooms.items() if v == Id]
     if keys:
         return keys[0]
     return None
 
-def get_room_id(RecorderName):
+def get_room_id(RecorderName):  # swap from room_id to room_name
     return rooms[RecorderName]
 
 async def get_room_state(room_name):    # include the soap request
@@ -86,11 +55,14 @@ async def get_room_state(room_name):    # include the soap request
     RemoteRecorder = RemoteRecorderManagement_client.call_service("GetRemoteRecordersById",remoteRecorderIds=get_room_id(room_name))
     RR_Name = RemoteRecorder[0]['Name']
     RR_State = RemoteRecorder[0]['State']
-    if RR_State == 'Recording':
+    if RR_State == 'Recording' and RemoteRecorder[0]['ScheduledRecordings'] is not None:
         live_recording_id = RemoteRecorder[0]['ScheduledRecordings']['guid'][0]
         return {"name": RR_Name, "state": RR_State, "recording_id": live_recording_id}
+    elif RemoteRecorder[0]['ScheduledRecordings'] is not None:
+        upcoming_recording = RemoteRecorder[0]['ScheduledRecordings']['guid'][0]
+        return {"name": RR_Name, "state": RR_State, "recording_id": upcoming_recording}
     else:
-        return {"name": RR_Name, "state": RR_State} # include next recording_id if available?
+        return {"name": RR_Name, "state": RR_State}
     return {"error": "room not found"}
 
 @app.get("/room_state/<string:room_name>")
@@ -108,16 +80,18 @@ def Get_SR_by_ID(sessionId):
 async def get_recording_information(room_name):     # include the necessary soap requests and data processing
     # await asyncio.sleep(2)  # simulation ~ will be removed
     RemoteRecorder = RemoteRecorderManagement_client.call_service("GetRemoteRecordersById",remoteRecorderIds=get_room_id(room_name))
+    RR_Name = RemoteRecorder[0]['Name']
+    RR_State = RemoteRecorder[0]['State']
     # print(RemoteRecorder[0]['ScheduledRecordings'])
-    # print(RemoteRecorder[0]['ScheduledRecordings'])
+    # print(RemoteRecorder[0]['ScheduledRecordings']==None)
     if RemoteRecorder[0]['State'] == "Recording":
         recording_id = RemoteRecorder[0]['ScheduledRecordings']['guid'][0]
         return {"id": recording_id, "Recording Information": Get_SR_by_ID(recording_id)}
     elif RemoteRecorder[0]['ScheduledRecordings'] is not None:
         upcoming_recording = RemoteRecorder[0]['ScheduledRecordings']['guid'][0]
-        RR_Name = RemoteRecorder[0]['Name']
-        RR_State = RemoteRecorder[0]['State']
         return {"name": RR_Name, "state": RR_State, "recording_id": upcoming_recording}
+    else:
+        return {"name": RR_Name, "state": RR_State}
     return {"error": "room not found"}
 
 @app.get("/recording_information/<string:room_name>")
@@ -129,17 +103,21 @@ def Start_Recording_By_RoomID(RoomID):
     SR_STARTTIME = datetime.datetime.now() + datetime.timedelta(hours=4)
     SR_ENDTIME = datetime.datetime.now() + datetime.timedelta(hours=4) + datetime.timedelta(hours=1)
     options = {"RecorderSettings":[{"RecorderId": RoomID}]}
-
-    Test_ScheduleRecording = RemoteRecorderManagement_client.call_service(
-        "ScheduleRecording",
-        name="Title of Recording in " + get_room_name(RoomID),
-        # folderId="418b7a1e-b39f-4f0e-8329-af32010abca9", # OPTIONAL, WILL USE DEFAULT FOLDER
-        isBroadcast="True",
-        start=SR_STARTTIME,
-        end=SR_ENDTIME,
-        recorderSettings=options)
-    print(Test_ScheduleRecording)
-    print("Recording has started.")
+    RemoteRecorder = RemoteRecorderManagement_client.call_service("GetRemoteRecordersById",remoteRecorderIds=RoomID)
+    if RemoteRecorder[0]['State'] != "Recording":
+        Test_ScheduleRecording = RemoteRecorderManagement_client.call_service(
+            "ScheduleRecording",
+            name="Title of Recording in " + get_room_name(RoomID),
+            # folderId="418b7a1e-b39f-4f0e-8329-af32010abca9", # OPTIONAL, WILL USE DEFAULT FOLDER
+            isBroadcast="True",
+            start=SR_STARTTIME,
+            end=SR_ENDTIME,
+            recorderSettings=options)
+        print(Test_ScheduleRecording)
+        return get_room_state(get_room_name(RoomID)) # Return POST success
+    else:
+        print("Recording in progress")
+        return get_room_state(get_room_name(RoomID)) # Return POST fail
 
 def Stop_Recording_By_RoomID(RoomID):
     # Check if a recording is taking place
@@ -154,8 +132,9 @@ def Stop_Recording_By_RoomID(RoomID):
             end=SR_ENDTIME)
         print(Test_ScheduleRecording)
         print("Recording has been stopped.")
+        return get_room_state(get_room_name(RoomID)) # Return POST success
     else:
-        return get_room_state(RoomID)
+        return get_room_state(get_room_name(RoomID)) # Return POST fail
 
 def Extend_5_Recording_By_RoomID(RoomID):
     # Check if a recording is taking place
@@ -172,8 +151,9 @@ def Extend_5_Recording_By_RoomID(RoomID):
             end=Calculated_EndTime+datetime.timedelta(minutes=5))
         print(Test_ScheduleRecording)
         print("Recording extended 5 minutes.")
+        return get_room_state(get_room_name(RoomID)) # Return POST success
     else:
-        return get_room_state(RoomID)
+        return get_room_state(get_room_name(RoomID)) # Return POST fail
 
 
 async def post_recording_state(room_name, data_request):
@@ -192,9 +172,34 @@ async def post_recording_state(room_name, data_request):
 @app.post("/recording_state/<string:room_name>")
 async def post_recording_state_route(room_name):
     data_request = request.get_data()
-    print(data_request)
+    request_dict = eval(data_request.decode("utf-8"))
+    print(request_dict)
+    print(type(request_dict))
+    match request_dict:
+        case {"requested_state": "recording"}:
+            print("Recording has Started")
+            print(room_name)
+            print(type(room_name))
+            # print(Start_Recording_By_RoomID(get_room_id(room_name)))
+            data = await Start_Recording_By_RoomID(get_room_id(room_name))
+            # await Start_Recording_By_RoomID(get_room_id(room_name))
+            return str(data).encode()
+        case {"requested_state": "stop"}:
+            print("Recording has Stopped")
+            data = await Stop_Recording_By_RoomID(get_room_id(room_name))
+            # print(Stop_Recording_By_RoomID(get_room_id(room_name)))
+            return str(data).encode()
+        case {"requested_state": "extend"}:
+            print("Recording has been Extended")
+            data = await Extend_5_Recording_By_RoomID(get_room_id(room_name))
+            # print(Extend_5_Recording_By_RoomID(get_room_id(room_name)))
+            return str(data).encode()
+        case _:
+            return {"error": "room not found"}
     #data = await post_recording_state(room_name,data_request)
     return data_request
 
-            
-    
+# Post Examples:
+# "POST","recording_state",{"requested_state": "recording"}
+# "POST","recording_state",{"requested_state": "stop"}
+# "POST","recording_state",{"requested_state": "extend"}
